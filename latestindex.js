5d7bd135de4d5cc1cd23ccf49e5ea2fdcc4e847e
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2023-12-13 00:18:26 +0530

    adding border and color to add items

---
+++ latestindex.js
@@ -0,0 +1,5 @@
+let headerMain=document.getElementById("main-header");
+headerMain.style.borderBottom="3px solid black";
+let addItem=document.getElementById("title");
+addItem.innerHTML="<b>Add Items</b>";
+addItem.style.color="green";
