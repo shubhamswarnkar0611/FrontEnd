706b20bad131efe3ef080b3143eca344170721a2
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2023-12-31 16:54:39 +0530

    get data and load on refresh

--- appointmentCrudCrud.js
+++ appointmentCrudCrud.js
@@ -1,83 +1,168 @@
-let expense = document.getElementById("addExpense");
-expense.addEventListener("submit", addExpense);
+// let expense = document.getElementById("addExpense");
+// expense.addEventListener("submit", addExpense);
+// getDataAndShowOnscreen();
+
+// function getDataAndShowOnscreen() {
+//   axios
+//     .get(
+//       "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment"
+//     )
+//     .then((res) => {
+//       console.log(res.data[0]);
+//       showUserExpenseToScreen(res.data);
+//     })
+//     .catch((err) => {
+//       console.log(err.msg);
+//     });
+// }
+
+// function addExpense(e) {
+//   e.preventDefault();
+//   let newExpense = document.getElementById("expense").value;
+//   let newDescription = document.getElementById("description").value;
+//   let newCategory = document.getElementById("category").value;
+
+//   let obj = {
+//     newExpense,
+//     newDescription,
+//     newCategory,
+//   };
+
+//   axios
+//     .post(
+//       "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment",
+//       obj
+//     )
+//     .then((res) => {
+//       console.log(res.data);
+//       showUserExpenseToScreen([res.data]);
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// }
+
+// function showUserExpenseToScreen(data) {
+//   console.log(data);
+//   //showung on screen code
+//   let parentElement = document.getElementById("parentEle");
+//   // childElement.className="list-group";
+
+//   data.map((item) => {
+//     //delete code
+//     let deleteChild = document.createElement("button");
+//     deleteChild.textContent = "Delete";
+//     deleteChild.className = "btn btn-danger  w-2";
+//     deleteChild.onclick = () => {
+//       localStorage.removeItem(obj.newDescription);
+//       parentElement.removeChild(childElement);
+//     };
+
+//     //edit code
+//     let editChild = document.createElement("button");
+//     editChild.textContent = "Edit";
+//     editChild.className = "btn btn-info";
+//     editChild.onclick = () => {
+//       localStorage.removeItem(obj.newDescription);
+//       parentElement.removeChild(childElement);
+
+//       document.getElementById("expense").value = obj.newExpense;
+//       document.getElementById("description").value = obj.newDescription;
+//       document.getElementById("category").value = obj.newCategory;
+//     };
+
+//     let childElement = document.createElement("li");
+//     const html_data = `<span class='exprense'>${item.newExpense}</span> <span class='description'>${item.newDescription}</span> <span class='category'>${item.newCategory}</span>`;
+//     console.log(item);
+//     console.log(html_data);
+//     childElement.innerHTML = html_data;
+//     childElement.appendChild(deleteChild);
+//     childElement.appendChild(editChild);
+//     parentElement.appendChild(childElement);
+//   });
+// }
+
+
+
+let expense= document.getElementById("addExpense");
+expense.addEventListener("submit",loadToLocalStorage);
 getDataAndShowOnscreen();
-
 function getDataAndShowOnscreen() {
-  axios
-    .get(
-      "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment"
-    )
-    .then((res) => {
-      console.log(res.data[0]);
-      showUserExpenseToScreen(res.data);
-    })
-    .catch((err) => {
-      console.log(err.msg);
-    });
-}
+    axios
+      .get(
+        "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment"
+      )
+      .then((res) => {
+       res.data.map((item)=>{
+        showUserExpenseToScreen(item)})
+      })
+      .catch((err) => {
+        console.log(err.msg);
+      });
+  }
 
-function addExpense(e) {
-  e.preventDefault();
-  let newExpense = document.getElementById("expense").value;
-  let newDescription = document.getElementById("description").value;
-  let newCategory = document.getElementById("category").value;
+function loadToLocalStorage(e) {
+    e.preventDefault();
+    let newExpense= document.getElementById("expense").value;
+    let newDescription = document.getElementById("description").value;
+    let newCategory= document.getElementById("category").value;  
 
-  let obj = {
-    newExpense,
-    newDescription,
-    newCategory,
-  };
+    let obj={
+        newExpense,
+        newDescription,
+        newCategory
+    };
 
-  axios
+    axios
     .post(
       "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment",
       obj
     )
     .then((res) => {
       console.log(res.data);
-      showUserExpenseToScreen([res.data]);
+      showUserExpenseToScreen(res.data);
     })
     .catch((err) => {
       console.log(err.message);
     });
-}
 
-function showUserExpenseToScreen(data) {
-  console.log(data);
-  //showung on screen code
-  let parentElement = document.getElementById("parentEle");
-  // childElement.className="list-group";
+    
+};
 
-  data.map((item) => {
+function showUserExpenseToScreen(obj) {
+    //showung on screen code
+    let parentElement = document.getElementById("parentEle");
+    let childElement = document.createElement("li");
+    // childElement.className="list-group";
+
+    childElement.textContent= obj.newExpense+"--"+obj.newDescription+"--"+obj.newCategory;
+
     //delete code
     let deleteChild = document.createElement("button");
-    deleteChild.textContent = "Delete";
-    deleteChild.className = "btn btn-danger  w-2";
-    deleteChild.onclick = () => {
-      localStorage.removeItem(obj.newDescription);
-      parentElement.removeChild(childElement);
+    deleteChild.textContent="Delete";
+    deleteChild.className="btn btn-danger  w-2";
+    deleteChild.onclick= ()=>{
+        localStorage.removeItem(obj.newDescription);
+        parentElement.removeChild(childElement);
     };
-
-    //edit code
-    let editChild = document.createElement("button");
-    editChild.textContent = "Edit";
-    editChild.className = "btn btn-info";
-    editChild.onclick = () => {
-      localStorage.removeItem(obj.newDescription);
-      parentElement.removeChild(childElement);
-
-      document.getElementById("expense").value = obj.newExpense;
-      document.getElementById("description").value = obj.newDescription;
-      document.getElementById("category").value = obj.newCategory;
-    };
-
-    let childElement = document.createElement("li");
-    const html_data = `<span class='exprense'>${item.newExpense}</span> <span class='description'>${item.newDescription}</span> <span class='category'>${item.newCategory}</span>`;
-    console.log(item);
-    console.log(html_data);
-    childElement.innerHTML = html_data;
     childElement.appendChild(deleteChild);
-    childElement.appendChild(editChild);
-    parentElement.appendChild(childElement);
-  });
-}
+
+   //edit code
+   let editChild = document.createElement("button");
+   editChild.textContent="Edit";
+   editChild.className="btn btn-info";
+   editChild.onclick=()=>{
+     localStorage.removeItem(obj.newDescription);
+     parentElement.removeChild(childElement);
+
+     document.getElementById("expense").value=obj.newExpense;
+     document.getElementById("description").value=obj.newDescription;
+     document.getElementById("category").value=obj.newCategory;  
+          
+    };
+
+   childElement.appendChild(editChild);
+
+
+   parentElement.appendChild(childElement);
+};
