95f9aa2af86e1c34a1fdfbf581ce9ab6849609f2
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2023-12-31 17:31:03 +0530

    delete from crud crud and remove from html

--- appointmentCrudCrud.js
+++ appointmentCrudCrud.js
@@ -1,88 +1,4 @@
-// let expense = document.getElementById("addExpense");
-// expense.addEventListener("submit", addExpense);
-// getDataAndShowOnscreen();
-
-// function getDataAndShowOnscreen() {
-//   axios
-//     .get(
-//       "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment"
-//     )
-//     .then((res) => {
-//       console.log(res.data[0]);
-//       showUserExpenseToScreen(res.data);
-//     })
-//     .catch((err) => {
-//       console.log(err.msg);
-//     });
-// }
-
-// function addExpense(e) {
-//   e.preventDefault();
-//   let newExpense = document.getElementById("expense").value;
-//   let newDescription = document.getElementById("description").value;
-//   let newCategory = document.getElementById("category").value;
-
-//   let obj = {
-//     newExpense,
-//     newDescription,
-//     newCategory,
-//   };
-
-//   axios
-//     .post(
-//       "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment",
-//       obj
-//     )
-//     .then((res) => {
-//       console.log(res.data);
-//       showUserExpenseToScreen([res.data]);
-//     })
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// }
-
-// function showUserExpenseToScreen(data) {
-//   console.log(data);
-//   //showung on screen code
-//   let parentElement = document.getElementById("parentEle");
-//   // childElement.className="list-group";
-
-//   data.map((item) => {
-//     //delete code
-//     let deleteChild = document.createElement("button");
-//     deleteChild.textContent = "Delete";
-//     deleteChild.className = "btn btn-danger  w-2";
-//     deleteChild.onclick = () => {
-//       localStorage.removeItem(obj.newDescription);
-//       parentElement.removeChild(childElement);
-//     };
-
-//     //edit code
-//     let editChild = document.createElement("button");
-//     editChild.textContent = "Edit";
-//     editChild.className = "btn btn-info";
-//     editChild.onclick = () => {
-//       localStorage.removeItem(obj.newDescription);
-//       parentElement.removeChild(childElement);
-
-//       document.getElementById("expense").value = obj.newExpense;
-//       document.getElementById("description").value = obj.newDescription;
-//       document.getElementById("category").value = obj.newCategory;
-//     };
-
-//     let childElement = document.createElement("li");
-//     const html_data = `<span class='exprense'>${item.newExpense}</span> <span class='description'>${item.newDescription}</span> <span class='category'>${item.newCategory}</span>`;
-//     console.log(item);
-//     console.log(html_data);
-//     childElement.innerHTML = html_data;
-//     childElement.appendChild(deleteChild);
-//     childElement.appendChild(editChild);
-//     parentElement.appendChild(childElement);
-//   });
-// }
-
-
+let url = "https://crudcrud.com/api/20e1059556f649c28c26cab35c829f03/appointment"
 
 let expense= document.getElementById("addExpense");
 expense.addEventListener("submit",loadToLocalStorage);
@@ -90,7 +6,7 @@ getDataAndShowOnscreen();
 function getDataAndShowOnscreen() {
     axios
       .get(
-        "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment"
+        url
       )
       .then((res) => {
        res.data.map((item)=>{
@@ -115,7 +31,7 @@ function loadToLocalStorage(e) {
 
     axios
     .post(
-      "https://crudcrud.com/api/5c55716ed02245a895fdf3b2eda0d603/appointment",
+      url,
       obj
     )
     .then((res) => {
@@ -141,8 +57,18 @@ function showUserExpenseToScreen(obj) {
     let deleteChild = document.createElement("button");
     deleteChild.textContent="Delete";
     deleteChild.className="btn btn-danger  w-2";
-    deleteChild.onclick= ()=>{
-        localStorage.removeItem(obj.newDescription);
+    deleteChild.id=obj._id
+
+    deleteChild.onclick=(event)=>{
+        axios
+        .delete(`${url}/${event.target.id}`)
+        .then(()=>{
+            alert('Deleted successfully');
+        })
+        .catch(error=>{
+            console.log(error.msg);
+        });
+
         parentElement.removeChild(childElement);
     };
     childElement.appendChild(deleteChild);
