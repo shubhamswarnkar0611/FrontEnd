d8f4c503e7e9c9370b06fc983b284f6d0701bae3
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-01-14 16:19:27 +0530

    get on crud crud

--- crudAppointmentBooking.js
+++ crudAppointmentBooking.js
@@ -1,6 +1,20 @@
-const url ="https://crudcrud.com/api/67e5415038794e4fb50cf4c12b3be0c1/appointment"
+const url ="https://crudcrud.com/api/9d9f56fb00b04187baee074915fd3b16/appointment"
 const add_post= document.getElementById("addPost");
 add_post.addEventListener("click",addPost);
+getRequestOnCrud();
+
+async function getRequestOnCrud(){
+ try{
+  let res = await axios.get(url);
+  console.log(res.data);
+  res.data.map((item) => {
+    showUserAppointmentToScreen(item);
+  });
+ }catch(err){
+  console.log(err);
+ }
+ 
+}
 
 
 async function addPost(e){
@@ -19,7 +33,7 @@ async function addPost(e){
 // console.log(obj);
 try{
 let post = await axios.post(url,obj); 
-showUserAppointmentToScreen(post); 
+showUserAppointmentToScreen(post.data); 
 }catch(err){
   console.log(err.message);
 
@@ -31,7 +45,7 @@ function showUserAppointmentToScreen(obj){
     let childElement=document.createElement("li"); 
 
    
-    childElement.textContent=`${obj.data.name}-${obj.data.phone}-${obj.data.email}`;
+    childElement.textContent=`${obj.name}-${obj.phone}-${obj.email}`;
 
     //delete
 
@@ -45,7 +59,7 @@ function showUserAppointmentToScreen(obj){
 
     //append
     
-    childElement.appendChild(deleteChild)
+    childElement.appendChild(deleteChild);
     
     childElement.appendChild(editChild);
     parentElement.appendChild(childElement);
